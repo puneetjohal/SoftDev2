70fce310296413e4f48b6cf0513970eeef2e01c2
Puneet Johal <pjohal@stuy.edu>
2019-03-13 13:05:00 -0400

    clear does not work

--- 09_svg/svg01.js
+++ 09_svg/svg01.js
@@ -1,6 +1,7 @@
 var isClean = true; //state var, tracks if SVG container is clean
 var xp; //x value of the previous circle
 var yp; //y value of the previous circle
+
 var pic = document.getElementById("vimage");
 
 pic.addEventListener( 'click', function(e) {
@@ -12,22 +13,30 @@ pic.addEventListener( 'click', function(e) {
   c.setAttribute("fill", "red");
   c.setAttribute("stroke", "black");
   pic.appendChild(c);
+  console.log("dot drawn")
 
   //connect the dots
-  if (isClean){
+  if (isClean) {
     isClean = false;
   }
   else {
-    var s = document.createElementNS("http://www.w3.org/2000/svg", stroke)
     var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
     l.setAttribute("x1", xp);
     l.setAttribute("y1", yp);
     l.setAttribute("x2", e.offsetX);
     l.setAttribute("y2", e.offsetY);
     l.setAttribute("stroke", "black");
-    l.setAttribute("stroke-width", 5);
     pic.appendChild(l);
+    console.log("line drawn")
   }
   xp = e.offsetX;
   yp = e.offsetY;
+});
+
+//clear button
+var button = document.getElementById("but_clear");
+
+button.addEventListener( 'click', function() {
+  pic.selectAll("*").remove();
+  console.log("cleared")
 });
