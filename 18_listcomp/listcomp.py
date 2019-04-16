7b0789954915980414208dece90bc49972545beb
Puneet Johal <pjohal@stuy.edu>
2019-04-16 14:47:37 -0400

    18

--- 18_listcomp/listcomp.py
+++ 18_listcomp/listcomp.py
@@ -1,17 +1,19 @@
+#Puneet Johal
+#SoftDev2 pd8
+#K18 -- Getting Clever with List Comprehensions
+#2019-04-15
+
 def pTriples(n):
   return [[x,y,z] for x in range(1,n) for y in range(1,n) for z in range(1,n) if x**2 + y**2 == z**2]
 print(pTriples(27))
 
-testList = [3,5,2,4,1]
-def qSortLoop(l):
-    for i in range(1,len(l)):
-        while i!=0 and l[i] < l[i-1]:
-            temp = l[i]
-            l[i] = l[i-1]
-            l[i-1] = temp
-            i -= 1
-    return l
-print(qSortLoop(testList))
 
 def qSort(l):
-    return []
+    if len(l) == 0 or len(l) == 1:
+        return l
+    pivot = int(len(l)/2) - 1
+    smaller = [x for x in l if x < l[pivot]]
+    larger = [x for x in l if x > l[pivot]]
+    return qSort(smaller) + [l[pivot]] + qSort(larger)
+testList = [7,3,5,8,2,4,1,9,6]
+print(qSort(testList))
